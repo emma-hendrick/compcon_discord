25e0998ce9b574fb9ef8300492b9ee5601486d0d
Emma Hendrick <mhendrick000@gmail.com>
2024-04-17 12:27:37 -0700

    Styles

--- bot/commands/get_character.py
+++ bot/commands/get_character.py
@@ -3,20 +3,36 @@ from bot.compcon.character_png import generate_png
 from bot.compcon.character_html import generate_html
 import discord
 from io import BytesIO
+import time
 
 #Get the character data and return it
 async def get_character_data(bot, channel, args):
+    
+    # Get start time
+    start_time = time.time()
 
     # Get the character json
     character_json = get_character_by_code(args[0])
-    print("json:", character_json)
+    json_time = time.time()
 
     # Convert the json into a character sheet using html
     html = generate_html(character_json)
+    html_time = time.time()
 
     # Convert the html into a png
     png = BytesIO(generate_png(html))
     filename = character_json["name"].lower().strip().replace(" ", "_")
+    png_time = time.time()
 
     # Send the file
     await channel.send(file=discord.File(png, filename=f"{filename}.png"))
+
+    # Get elapsed time for each step
+    elapsed_json = json_time - start_time
+    elapsed_html = html_time - json_time
+    elapsed_png = png_time - html_time
+
+    # Print time for all tasks
+    print(f"JSON: {elapsed_json}")
+    print(f"HTML: {elapsed_html}")
+    print(f"PNG: {elapsed_png}")
--- bot/compcon/character_html.py
+++ bot/compcon/character_html.py
@@ -13,6 +13,87 @@ def generate_html(character):
     current_hp = int(character["current_hp"])
     hp_line = f"{current_hp} / {max_hp} HP"
 
+    # CSS
+    css = """
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f2f2f2;
+                    margin: 0;
+                    padding: 20px;
+                }
+
+                header {
+                    margin-bottom: 20px;
+                }
+
+                header h1 {
+                    color: #333;
+                }
+
+                header h2 {
+                    margin-top: 5px;
+                }
+
+                h2 {
+                    color: #a01717;
+                }
+
+                p {
+                    color: #999;
+                }
+
+                /* Style for character details */
+                .character-details {
+                    margin-bottom: 20px;
+                }
+
+                .character-details h2 {
+                    font-size: 18px;
+                }
+
+                .character-details p {
+                    font-size: 16px;
+                    margin: 5px 0;
+                }
+
+                /* Style for character history */
+                .character-history {
+                    margin-bottom: 20px;
+                }
+
+                .character-history h2 {
+                    font-size: 18px;
+                }
+
+                .character-history p {
+                    font-size: 16px;
+                    line-height: 1.5;
+                }
+
+                /* Style for character skills and talents */
+                .character-skills-talents {
+                    margin-bottom: 20px;
+                }
+
+                .character-skills-talents h2 {
+                    font-size: 18px;
+                }
+
+                .character-skills-talents h3 {
+                    font-size: 16px;
+                    color: #666;
+                }
+
+                .character-skills-talents ul {
+                    padding-left: 20px;
+                }
+
+                ul {
+                    list-style: none;
+                }
+            """
+    css_html = f"<style>{css}</style>"
+
     # Head elements
     title = f"<title>{charname_line}</title>"
     
@@ -29,32 +110,40 @@ def generate_html(character):
     <head>
         {title}
         {meta}
+        {css_html}
     </head>
     """
 
     # Body elements
-    header = f"<h1>{charname_line}</h1>"
-    background = f"<h4>{character['background']}</h4>"
-    subheader = f"<h2>Level {level}</h2>"
-    hp = f"<h4>{hp_line}</h4>"
-    description_html = f"<p>{description}</p>"
+    charname = f"<h1>{charname_line}</h1>"
+    subheader = f"<h2>Level {level} - {hp_line} - {character['background']}</h2>"
     stati = f"<p>Status: {character['status']} <br>Dead: {character['dead']}</p>"
-    notes = f"<p>Notes: {character['notes']}</p>"
-    history = f"<h4>History</h4>{character['history']}"
+    description_html = f"<h2>Description</h2><p>{description}</p>"   
+    notes = f"<h2>Notes</h2><p>{character['notes']}</p>"
+    history = f"<h2>History</h2>{character['history']}"
+
+    # Header
+    header = f"<header>{charname}{subheader}</header>"
+
+    # Character details
+    character_details = f"<section class='character-details'>{stati}{description_html}{notes}</section>"
+
+    # History Block
+    history_block = f"<section class='character-history'>{history}</section>"
 
     # Skills
     skills = character["skills"]
     skill_content = ""
     for skill in skills:
-        skill_content += f"{skill['id']} {skill['rank']}<br>"
-    skill_html = f"<h4>Skills</h4><p>{skill_content}</p>"
+        skill_content += f"<li>{skill['id']} {skill['rank']}</li>"
+    skill_html = f"<h3>Skills</h3><ul>{skill_content}</ul>"
 
     # Talents
     talents = character["talents"]
     talent_content = ""
     for talent in talents:
-        talent_content += f"{talent['id']} {talent['rank']}<br>"
-    talent_html = f"<h4>Talents</h4><p>{talent_content}</p>"
+        talent_content += f"<li>{talent['id']} {talent['rank']}</li>"
+    talent_html = f"<h3>Talents</h3><ul>{talent_content}</ul>"
 
     # Mech Skills
     mech_skills = character["mechSkills"]
@@ -66,24 +155,20 @@ def generate_html(character):
         "Engineering"
     ]
     for index, mech_skill in enumerate(mech_skills):
-        mech_skill_content += f"{mech_skill_names[index]}: {mech_skill}<br>"
-    mech_skill_html = f"<h4>Mech Skills</h4><p>{mech_skill_content}</p>"
+        mech_skill_content += f"<li>{mech_skill_names[index]}: {mech_skill}</li>"
+    mech_skill_html = f"<h3>Mech Skills</h3><ul>{mech_skill_content}</ul>"
+
+    # Skills and talents block
+    skills_talent_block = f"<section class='character-skills-talents'><h2>Skills and Talents</h2>{skill_html}{talent_html}{mech_skill_html}</section>"
 
     # Amalgamate the body
     body = f"""
     <body>
         {header}
-        {background}
-        {subheader}
-        {hp}
-        {description_html}
-        {stati}
-        {notes}
-        {history}
+        {character_details}
+        {history_block}
 
-        {skill_html}
-        {talent_html}
-        {mech_skill_html}
+        {skills_talent_block}
     </body>
     """
 
@@ -94,6 +179,6 @@ def generate_html(character):
         {body}
     </html>
     """
-
+    
     # Return the HTML
     return html
